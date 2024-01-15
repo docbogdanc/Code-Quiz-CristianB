8c9a17169e44a3b8c98a48eda0900c707ba88fbc
docbogdanc <docbogdanc@yahoo.com>
2024-01-15 19:42:02 +0000

    added function to reset question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,7 +2,7 @@ var startButton = document.querySelector("#start");
 var startScreen = document.querySelector("#start-screen")
 var timeEl = document.querySelector("#time");
 var questionsEl = document.querySelector("#questions");
-
+var secondLeft=1000;
 
 
 startButton.addEventListener('click', startQuestions)
@@ -10,11 +10,11 @@ function startQuestions(){
 // replace the class to hide the start menu and show the question
     startScreen.classList.replace('start','hide');
     questionsEl.classList.replace('hide','show')
-    questions();
+    randomQuestion()
+    resetToNext()
 }
 
-// create a variable for timer start seconds
-var secondLeft=1000;
+
 // create a funtion for timer
 function timer () {
     var setTimer = setInterval(function() {
@@ -29,7 +29,7 @@ function timer () {
     },1000);
 }
 timer();
-
+localStorage.setItem("time", secondLeft);
 function finalMessage () {
     alert("The End!");
 }
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,11 +1,12 @@
 var questionTitleEl = document.querySelector("#question-title");
 var choicesEl = document.querySelector("#choices");
 var unordListEl = document.querySelector("#list");
-
 // create starting point for score
 var score = 0;
 
 var storageScore = localStorage.getItem("count");
+// var timeRemaining = localStorage.getItem("time");
+// var timeRemaining = 0;
 
 // create an array of questions as objects
 var arrayQuestions = [
@@ -31,128 +32,129 @@ var arrayQuestions = [
  answer4: "extra"
 }
 ]
-var correctChoice = ["there", "green", "not"];
+var correctChoice = [arrayQuestions[0].answer2, "green", "not", "not"];
 
-
-    
 // calculate the number of objects/questions from the array
 var arrayLength = arrayQuestions.length
-// create a function to choose a rondomm object/question from the array
-var qIndex = Math.floor(Math.random()*3);
-console.log("random number of question is qIndex= "+qIndex);
 
-// select an object/question from the array
-var currentQuestion = arrayQuestions[qIndex]
-console.log("current question index is "+currentQuestion);
-// create an array with the values of the object/question
-var currentChoices = Object.values(currentQuestion);
-console.log("values are: "+currentChoices);
+function randomQuestion () {
+  // create a function to choose a rondomm object/question from the array
+  var qIndex = Math.floor(Math.random()*3);
 
-// create the function to display the question title
-function questions () {
-    questionTitleEl.textContent = currentChoices[0];
-}
-
-// create a function to create new buttons and display the question choices in them
-function unListChoices() {
-  var newListItem
-// create a for loop to return the number of values of the object/question
-  for (let i = 1; i < currentChoices.length; i++) {
-//   create new element list as buttons and add the question' choice text inside them ; append the buttons to "ul" parent element 
-    newListItem = document.createElement('button');
-    newListItem.setAttribute('id', "button"+i)
-    unordListEl.appendChild(newListItem);
-    newListItem.textContent = i+". "+currentChoices[i];
-    console.log("newlistitem is "+newListItem);
+  // select an object/question from the array
+  var currentQuestion = arrayQuestions[qIndex]
+  
+  // create an array with the values of the object/question
+  var currentChoices = Object.values(currentQuestion);
+  
+  // create the function to display the question title
+  function questions () {
+      questionTitleEl.textContent = currentChoices[0];
   }
-//   create variable for each button element based on their "id"
-  var button1 = document.getElementById("button1");
-  var button2 = document.getElementById("button2");
-  var button3 = document.getElementById("button3");
-  var button4 = document.getElementById("button4");
   
- 
-  console.log("the correct answer is "+correctChoice[qIndex]);
-
-// create event listener for click on buttons ; score is increasing if correct, time is dicreasing if incorrect
-  button1.addEventListener("click", function() {
-    if (button1.textContent === correctChoice[qIndex]) {
-        score++;
-        // console.log("score is: "+score);
-        return score
-    } else {
-        if (score>0) { 
-            score --;
-          } else (score=0);
-        if (secondLeft>=10) {
-        secondLeft -= 10;} else {
-            secondLeft=0;}
-        // console.log("score is: "+score);
-        // return score
-}
-localStorage.setItem("count", score);
-});  
-  
-  button2.addEventListener("click", function() {
-    if (button2.textContent === ("2. "+correctChoice[qIndex])) {
-        score++;
-        // console.log("score is: "+score);
-        // return score
-    } else {
-        if (score>0) { 
-            score --;
-          } else (score=0);
-        if (secondLeft>=10) {
-        secondLeft -= 10;} else {
-            secondLeft=0;}
-        // console.log("score is: "+score);
-        // return score
-  }
-  localStorage.setItem("count", score);
-})
-  
-  button3.addEventListener("click", function() {
-    if (button3.textContent === correctChoice[qIndex]) {
-        score++;
-        // console.log("score is: "+score);
-        // return score;
-    } else {
-        if (score>0) { 
-            score --;
-          } else (score=0);
-        if (secondLeft>=10) {
-        secondLeft -= 10;} else {
-            secondLeft=0;}
-        // console.log("score is: "+score);
-        // return score;
+  // create a function to create new buttons and display the question choices in them
+  function unListChoices() {
+    var newListItem
+  // create a for loop to return the number of values of the object/question
+    for (let i = 1; i < currentChoices.length; i++) {
+  //   create new element list as buttons and add the question' choice text inside them ; append the buttons to "ul" parent element 
+      newListItem = document.createElement('button');
+      newListItem.setAttribute('id', "button"+i)
+      unordListEl.appendChild(newListItem);
+      newListItem.textContent = i+". "+currentChoices[i];
+      // console.log("newlistitem is "+newListItem);
     }
-    localStorage.setItem("count", score);
-})
-
-  button4.addEventListener("click", function() {  
-      if (button4.textContent === correctChoice[qIndex]) {
+  //   create variable for each button element based on their "id"
+    var button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
+    var button3 = document.getElementById("button3");
+    var button4 = document.getElementById("button4");
+    
+   
+  //   console.log("the correct answer is "+correctChoice[qIndex]);
+  
+  // create event listener for click on buttons ; score is increasing if correct, time is dicreasing if incorrect
+    button1.addEventListener("click", function() {
+      if (button1.textContent ===("1. "+ correctChoice[qIndex])) {
           score++;
-        //   console.log("score is: "+score);
-        //   return score;
+          // console.log("score is: "+score);
+          return score
       } else {
-          console.log("wrong answer"+ " Score is "+score);
-          if (score>0) { 
-            score --;
-          } else (score=0);
+  
           if (secondLeft>=10) {
           secondLeft -= 10;} else {
               secondLeft=0;}
-        //   console.log("score is: "+score);
-        //   return score;
-      }; 
-      console.log("semi final score: " + score);
+          // console.log("score is: "+score);
+          // return score
+  }
+  // delete current question to prepare for next one
+  resetToNext();
+  localStorage.setItem("count", score); //send the score to local storage
+  });  
+    
+    button2.addEventListener("click", function() {
+      if (button2.textContent === ("2. "+correctChoice[qIndex])) {
+          score++;
+          // console.log("score is: "+score);
+          // return score
+      } else {
+  
+          if (secondLeft>=10) {
+          secondLeft -= 10;} else {
+              secondLeft=0;}
+          // console.log("score is: "+score);
+          // return score
+    }
+    resetToNext();
+    localStorage.setItem("count", score);
+  })
+    
+    button3.addEventListener("click", function() {
+      if (button3.textContent ===("3. "+ correctChoice[qIndex])) {
+          score++;
+          // console.log("score is: "+score);
+          // return score;
+      } else {
+          if (secondLeft>=10) {
+          secondLeft -= 10;} else {
+              secondLeft=0;}
+          // console.log("score is: "+score);
+          // return score;
+      }
       localStorage.setItem("count", score);
-      return score; 
-    })
- 
-   }
-unListChoices()
+      resetToNext();
+  })
+  
+    button4.addEventListener("click", function() {  
+        if (button4.textContent ===("4. "+ correctChoice[qIndex])) {
+            score++;
+          //   console.log("score is: "+score);
+          //   return score;
+        } else {
+            if (secondLeft>=10) {
+            secondLeft -= 10;} else {
+                secondLeft=0;}
+        }; 
+        console.log("semi final score: " + score);
+        localStorage.setItem("count", score);
+        // timeRemaining = 0
+        
+        resetToNext();
+        return score; 
+      })
+   
+     }
+  unListChoices()
+
+}
 
 
+function resetToNext() {
+    console.log("stergem!");
+    questionTitleEl.innerHTML="";
+    // removing all buttons by 
+    unordListEl.innerHTML = ""; 
+}
 
-console.log("final score is: "+score);
+
+// console.log("final score is: "+score);
