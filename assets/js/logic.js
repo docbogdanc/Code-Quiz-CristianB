9e06fb81f5ebf50e608f96aa2f10ba002a19bf16
docbogdanc <docbogdanc@yahoo.com>
2024-01-15 10:23:55 +0000

    time is going dow with wrong choice but only with the 3rd one... why...need debugging

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,32 +7,29 @@ var questionsEl = document.querySelector("#questions");
 
 startButton.addEventListener('click', startQuestions)
 function startQuestions(){
-// replace the class to hide the start menu
+// replace the class to hide the start menu and show the question
     startScreen.classList.replace('start','hide');
     questionsEl.classList.replace('hide','show')
     questions();
 }
 
-// create a variable for timer seconds
+// create a variable for timer start seconds
 var secondLeft=100;
-
-// create funtion for timer
+// create a funtion for timer
 function timer () {
     var setTimer = setInterval(function() {
         secondLeft --;
         timeEl.textContent = secondLeft;
 
-        if (secondLeft ===0) {
+        if (secondLeft <= 0) {
+            secondLeft = 0
             clearInterval(setTimer);
             finalMessage();
         }
     },1000);
 }
+timer();
 
 function finalMessage () {
     alert("The End!");
 }
-
-
-
-timer();
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,62 +1,79 @@
 var questionTitleEl = document.querySelector("#question-title");
 var choicesEl = document.querySelector("#choices");
 var unordListEl = document.querySelector("#list");
-var choice1_El = document.querySelector("#choice1");
-var choice2_El = document.querySelector("#choice2");
-var choice3_El = document.querySelector("#choice3");
-var choice4_El = document.querySelector("#choice4");
 
+// create starting point for score
+var Score = 0;
 
-   // create an array of questions as objects
-   var arrayQuestions = [
-   {
-      title: "Where is ....",
-      answer1: "here",
-      answer2: "there",
-      answer3: "nowhere"
-   },
-   {
-       title: "What is ....",
-       answer1: "blue",
-       answer2: "green",
-       answer3: "white" 
-   },
-   {
-    title : "Why is ....",
-    answer1: "cause",
-    answer2: "not",
-    answer3: "mmmh" 
-   }
+// create an array of questions as objects
+var arrayQuestions = [
+{
+   title: "Where is ....",
+   answer1: "here",
+   answer2: "there",
+   answer3: "nowhere"
+},
+{
+    title: "What is ....",
+    answer1: "blue",
+    answer2: "green",
+    answer3: "white" 
+},
+{
+ title : "Why is ....",
+ answer1: "cause",
+ answer2: "not",
+ answer3: "mmmh" 
+}
 ]
+var correctChoice = ["there", "green", "not"];
+
+
+    
+// calculate the number of objects/questions from the array
+var arrayLength = arrayQuestions.length
+// create a function to choose a rondomm object/question from the array
+var qIndex = Math.floor(Math.random()*3);
+console.log("random number of question is "+qIndex);
+
+// select an object/question from the array
+var currentQuestion = arrayQuestions[qIndex]
+console.log("current question index is "+currentQuestion);
+// create an array with the values of the object/question
+var currentChoices = Object.values(currentQuestion);
+console.log("values are "+currentChoices);
 
 // create the function to display the question title
 function questions () {
     questionTitleEl.textContent = currentChoices[0];
-//     //  choicesEl.innerText = qChoice;
-    }
-    
-// calculate the number of questions from the array
-var arrayLength = arrayQuestions.length
-// create a function to create an unordered list inside de "choices" div
-var qIndex = Math.floor(Math.random()*3);
-console.log(qIndex);
-
-// select a question (object) from the array
-var currentQuestion = arrayQuestions[qIndex]
-console.log(currentQuestion);
-// create an array with the values of the object (question)
-var currentChoices = Object.values(currentQuestion);
-console.log("values are "+currentChoices);
-
-// create a function to create question choices and display them
-var qIndex = 0;
-function unListChoices() {
-// create a for loop to return the number of keys of the object
-for (let i = 1; i < currentChoices.length; i++) {
-//   create new element list and add choice text 
-  var newListItem = document.createElement('button');
-  unordListEl.appendChild(newListItem);
-  newListItem.textContent = i+". "+currentChoices[i];
 }
+
+// create a function to create new buttons and display the question choices in them
+function unListChoices() {
+// create a for loop to return the number of values of the object/question
+  for (let i = 1; i < currentChoices.length; i++) {
+//   create new element list and add the question choice text 
+    var newListItem = document.createElement('button');
+    unordListEl.appendChild(newListItem);
+    newListItem.textContent = i+". "+currentChoices[i];
+  }
+
+  newListItem.addEventListener('click', function(event) {
+    if (event.target.textContent === correctChoice[qIndex]) {
+        console.log("the correct answer is "+correctChoice[qIndex]);
+        console.log("your answer is "+event.target.textContent);
+        Score++;
+    } else {
+        Score --;
+        // reduce time remaining with 10 if wrong answer
+        if (secondLeft>=10) {
+        secondLeft -= 10;} else {
+            secondLeft=0;}
+        }
+    }
+)
+
 }
 unListChoices()
+
+
