d5617702cc304f5762c95d7947934f543fcf61dc
docbogdanc <docbogdanc@yahoo.com>
2024-01-15 20:42:23 +0000

    final screen finished with initial send to local data - need working highscore page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,16 +1,20 @@
 var startButton = document.querySelector("#start");
-var startScreen = document.querySelector("#start-screen")
+var startScreen = document.querySelector("#start-screen");
+var endScreen = document.querySelector("#end-screen");
+var finalScore = document.querySelector("#final-score");
+var initials = document.querySelector("#initials");
+var submitButton = document.querySelector("#submit");
 var timeEl = document.querySelector("#time");
 var questionsEl = document.querySelector("#questions");
 var secondLeft=10;
 
 
-startButton.addEventListener('click', startQuestions)
+startButton.addEventListener('click', startQuestions);
 function startQuestions(){
 // replace the class to hide the start menu and show the question
     startScreen.classList.replace('start','hide');
-    questionsEl.classList.replace('hide','show')
-    randomQuestion()
+    questionsEl.classList.replace('hide','show');
+    randomQuestion();
     // resetToNext()
     
 }
@@ -34,10 +38,18 @@ function timer () {
 }
 timer();
 
+
 function finalMessage () {
     resetToNext()
     questionsEl.classList.replace('show', 'hide');
-    alert("You finished with the score: "+score)
+    endScreen.classList.replace('hide', 'show');
+    submitButton.addEventListener('click', sendInitials)
+    finalScore.textContent = score;
+}
+
+function sendInitials() {
+    var inputData = initials.value;
+    localStorage.setItem("initials", inputData);
+    endScreen.classList.replace('show', 'hide');
     startScreen.classList.replace('hide', 'start');
-    
 }
