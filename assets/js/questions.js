7e4b9e1b314c187fe457448aa122b31db9d2da1f
docbogdanc <docbogdanc@yahoo.com>
2024-01-15 17:57:26 +0000

    local storage added for score

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,6 +5,8 @@ var unordListEl = document.querySelector("#list");
 // create starting point for score
 var score = 0;
 
+var storageScore = localStorage.getItem("count");
+
 // create an array of questions as objects
 var arrayQuestions = [
 {
@@ -56,104 +58,101 @@ function unListChoices() {
   var newListItem
 // create a for loop to return the number of values of the object/question
   for (let i = 1; i < currentChoices.length; i++) {
-//   create new element list and add the question choice text 
+//   create new element list as buttons and add the question' choice text inside them ; append the buttons to "ul" parent element 
     newListItem = document.createElement('button');
     newListItem.setAttribute('id', "button"+i)
     unordListEl.appendChild(newListItem);
     newListItem.textContent = i+". "+currentChoices[i];
     console.log("newlistitem is "+newListItem);
   }
+//   create variable for each button element based on their "id"
   var button1 = document.getElementById("button1");
   var button2 = document.getElementById("button2");
   var button3 = document.getElementById("button3");
   var button4 = document.getElementById("button4");
   
-  console.log("newListItem is "+newListItem);
-  console.log("correct answer is: "+correctChoice[qIndex]);
-//   newListItem.addEventListener('click', function() {
+ 
   console.log("the correct answer is "+correctChoice[qIndex]);
 
+// create event listener for click on buttons ; score is increasing if correct, time is dicreasing if incorrect
   button1.addEventListener("click", function() {
-
     if (button1.textContent === correctChoice[qIndex]) {
-        console.log("the correct answer is "+correctChoice[qIndex]);
-
         score++;
-        console.log("score is: "+score);
+        // console.log("score is: "+score);
         return score
     } else {
-        console.log("wrong answer"+ " Score is "+score);
-        score --;
-        // reduce time remaining with 10 if wrong answer
+        if (score>0) { 
+            score --;
+          } else (score=0);
         if (secondLeft>=10) {
         secondLeft -= 10;} else {
             secondLeft=0;}
-        console.log("score is: "+score);
-        return score
-        }
-  })
+        // console.log("score is: "+score);
+        // return score
+}
+localStorage.setItem("count", score);
+});  
   
   button2.addEventListener("click", function() {
-
-    console.log("you pressed button2");
-    console.log(button2.textContent);
-    console.log("2. "+correctChoice[qIndex]);
     if (button2.textContent === ("2. "+correctChoice[qIndex])) {
-        console.log("the correct answer is "+correctChoice[qIndex]);
-        console.log("your answer is "+button2.textContent);
         score++;
-        console.log("score is: "+score);
-        return score
-       
+        // console.log("score is: "+score);
+        // return score
     } else {
-        console.log("wrong answer"+ " Score is "+score);
-        score --;
-        // reduce time remaining with 10 if wrong answer
+        if (score>0) { 
+            score --;
+          } else (score=0);
         if (secondLeft>=10) {
         secondLeft -= 10;} else {
             secondLeft=0;}
-        console.log("score is: "+score);
-        return score
-        }
-  })
+        // console.log("score is: "+score);
+        // return score
+  }
+  localStorage.setItem("count", score);
+})
   
   button3.addEventListener("click", function() {
-
     if (button3.textContent === correctChoice[qIndex]) {
-        console.log("the correct answer is "+correctChoice[qIndex]);
-     
         score++;
-        console.log("score is: "+score);
-        return score;
+        // console.log("score is: "+score);
+        // return score;
     } else {
-        console.log("wrong answer"+ " Score is "+score);
-        score --;
-        // reduce time remaining with 10 if wrong answer
+        if (score>0) { 
+            score --;
+          } else (score=0);
         if (secondLeft>=10) {
         secondLeft -= 10;} else {
             secondLeft=0;}
-        console.log("score is: "+score);
-        return score;
-    }})
+        // console.log("score is: "+score);
+        // return score;
+    }
+    localStorage.setItem("count", score);
+})
+
   button4.addEventListener("click", function() {  
       if (button4.textContent === correctChoice[qIndex]) {
-          console.log("the correct answer is "+correctChoice[qIndex]);
-       
           score++;
-          console.log("score is: "+score);
-          return score;
+        //   console.log("score is: "+score);
+        //   return score;
       } else {
           console.log("wrong answer"+ " Score is "+score);
-          score --;
-          // reduce time remaining with 10 if wrong answer
+          if (score>0) { 
+            score --;
+          } else (score=0);
           if (secondLeft>=10) {
           secondLeft -= 10;} else {
               secondLeft=0;}
-          console.log("score is: "+score);
-          return score;
-      }})
+        //   console.log("score is: "+score);
+        //   return score;
+      }; 
+      console.log("semi final score: " + score);
+      localStorage.setItem("count", score);
+      return score; 
+    })
+ 
    }
-
-console.log("score is: "+score);
-
 unListChoices()
+
+
+
+console.log("final score is: "+score);
